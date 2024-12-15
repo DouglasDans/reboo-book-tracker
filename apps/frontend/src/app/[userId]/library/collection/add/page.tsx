f367c5ba390f6919fffc90707386a43e22d7c070
douglasdans <douglas.dans@gmail.com>
2024-12-15 16:37:11 -0300

    feat(frontend | collection-add-page): adiciona página para adicionar novas coleções

---
+++ apps/frontend/src/app/[userId]/library/collection/add/page.tsx
@@ -0,0 +1,9 @@
+import CollectionAddPage from '@/containers/library-page/collection-page/main-page/add-page'
+import React from 'react'
+
+
+export default function page() {
+  return (
+    <CollectionAddPage />
+  )
+}
