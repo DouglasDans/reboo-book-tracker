1dbaa3860b44951d89e7021232d871baaa0b0f80
douglasdans <douglas.dans@gmail.com>
2024-12-16 17:06:27 -0300

    feat(frontend | forms-types): adiciona interfaces FormBook e FormCollection para gerenciamento de estado de formulários

---
+++ apps/frontend/src/types/forms.types.ts
@@ -0,0 +1,28 @@
+export interface FormBook {
+  id?: number
+  title: string
+  authors: string
+  publicationDate?: string
+  publisher?: string
+  totalPages: number
+  isbn?: string
+  categories?: string
+  language: string
+  description: string
+
+  status: "BUY" | "NOT_STARTED" | "IN_PROGRESS" | "COMPLETED" | "GIVEN_UP"
+  pagesRead: number
+
+  coverImage: string
+  highlightColor: string
+  userId: number
+}
+
+export interface FormCollection {
+  id?: number
+  name: string
+  backgroundColors: string
+  userId: number
+
+  books?: Array<number>
+}
