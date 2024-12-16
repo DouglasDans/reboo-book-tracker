4b8270a84c3aa9a47974f3e01ecc16815b06d469
douglasdans <douglas.dans@gmail.com>
2024-12-16 17:40:20 -0300

    refactor(frontend | collection-add-page): renomeia função de exportação para PageCollectionAdd

--- apps/frontend/src/app/[userId]/library/collection/add/page.tsx
+++ apps/frontend/src/app/[userId]/library/collection/add/page.tsx
@@ -1,8 +1,7 @@
 import CollectionAddPage from '@/containers/library-page/collection-page/main-page/add-page'
 import React from 'react'
 
-
-export default function page() {
+export default function PageCollectionAdd() {
   return (
     <CollectionAddPage />
   )
