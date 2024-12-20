7ec9101d5a3dae86887efbf88b5e306f2a5d9457
douglasdans <douglas.dans@gmail.com>
2024-12-20 13:45:46 -0300

    feat(frontend | input-color-selector): adiciona color picker menu funcional com atualização direta no contexto do estado

--- apps/frontend/src/components/forms/input-color-selector/index.tsx
+++ apps/frontend/src/components/forms/input-color-selector/index.tsx
@@ -5,22 +5,22 @@ import Icon from '@/components/icon'
 import ToggleDropdownMenu from '@/components/toggle-dropdown-menu'
 import ColorPickerMenu from '@/components/toggle-dropdown-menu/dropdown-menus/ColorPickerMenu'
 
-type Props = {
+type Props<T> = {
   title?: string
-  color: string
-  setState?: () => void
-  value?: string | number
+  name: string
+  setState: (data: Partial<T>) => void
+  value: string
 }
 
-export default function InputColorSelector({ title = "Cor de Destaque" }: Props) {
+export default function InputColorSelector<T>({ title = "Cor de Destaque", name, setState, value }: Props<T>) {
   return (
     <div className={styles.container}>
       <label>
         {title}
       </label>
       <div className={styles.inputContainer}>
-        <div className={styles.input} style={{ backgroundColor: "fff" }}></div>
-        <ToggleDropdownMenu content={"yet"}>
+        <div className={styles.input} style={{ backgroundColor: value }}></div>
+        <ToggleDropdownMenu content={<ColorPickerMenu name={name} setState={setState} value={value} />}>
           <Button notRounded variant='secondary' startDecorator={<Icon name='palette' />}>
             Selecionar
           </Button>
