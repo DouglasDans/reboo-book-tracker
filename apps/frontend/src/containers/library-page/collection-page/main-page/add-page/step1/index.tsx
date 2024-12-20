2653aa29a5fa47aa3ce22ad5baad6b082207175a
douglasdans <douglas.dans@gmail.com>
2024-12-20 13:46:12 -0300

    feat(frontend | step1-collection-data): integra estado do formulário ao componente Step1CollectionData com gerenciamento de mudanças

--- apps/frontend/src/containers/library-page/collection-page/main-page/add-page/step1/index.tsx
+++ apps/frontend/src/containers/library-page/collection-page/main-page/add-page/step1/index.tsx
@@ -1,15 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import styles from './index.module.scss'
 import Input from '@/components/forms/input'
 import InputColorSelector from '@/components/forms/input-color-selector'
+import { MenuFormData } from '@/context/tabbed-menu-layout/MenuFormDataProvider'
+import { FormCollection } from '@/types/forms.types'
+import { createHandleChange } from '@/utils/form.utils'
 
 export default function Step1CollectionData() {
+  const [collectionState, setCollectionState] = MenuFormData.useMenuFormData<FormCollection>()
+  const handleChange = createHandleChange(setCollectionState)
+
   return (
     <Fragment>
       <h6>Informações da Coleção</h6>
       <div className={styles.formContainer}>
-        <Input type='text' title='Nome da Coleção' placeholder='Digite o nome da coleção' />
-        <InputColorSelector color="#fff" />
+        <Input
+          type='text'
+          title='Nome da Coleção'
+          placeholder='Digite o nome da coleção'
+          name='name'
+          value={collectionState.name}
+          onChange={handleChange}
+        />
+        <InputColorSelector
+          value={collectionState.backgroundColors}
+          name='backgroundColors'
+          setState={setCollectionState}
+        />
       </div>
     </Fragment>
   )
