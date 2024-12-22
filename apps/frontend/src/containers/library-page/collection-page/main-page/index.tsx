134ec7a13fbd2492835695ca29c48722b009743e
douglasdans <douglas.dans@gmail.com>
2024-12-22 12:01:31 -0300

    fix(frontend | collection-page): corrige posicionamento do ListView

--- apps/frontend/src/containers/library-page/collection-page/main-page/index.tsx
+++ apps/frontend/src/containers/library-page/collection-page/main-page/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CollectionBannerContainer from './banner-container'
 import { Book, Collection } from '@/api/reboo-api/api.types'
 import ListItems from '@/components/list-items'
+import styles from './index.module.scss'
 
 type Props = {
   collection: Collection
@@ -11,7 +12,7 @@ export default function CollectionPage({ collection }: Props) {
   const books: Book[] = collection.books?.map(item => item.book) || [];
 
   return (
-    <div>
+    <div className={styles.container}>
       <CollectionBannerContainer {...collection} />
       <ListItems
         title='Livros Adicionados'
