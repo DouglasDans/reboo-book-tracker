d87a0c1b57f57f79b3e3cb2a0c77f9c7db928dd5
douglasdans <douglas.dans@gmail.com>
2024-12-23 09:23:38 -0300

    feat(frontend | collection-banner): formata datas de criação e edição no banner da coleção

--- apps/frontend/src/containers/library-page/collection-page/main-page/banner-container/index.tsx
+++ apps/frontend/src/containers/library-page/collection-page/main-page/banner-container/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './index.module.scss'
 import Button from '@/components/buttons/button'
 import Icon from '@/components/icon'
+import { formatDate } from '@/utils/library.utils'
 
 type Props = {
   name: string
@@ -11,18 +12,21 @@ type Props = {
 }
 
 export default function CollectionBannerContainer({ name, backgroundColors, createdAt, updatedAt }: Props) {
+  const createdAtFormatted = formatDate(createdAt)
+  const updatedAtFormatted = formatDate(updatedAt)
+
   return (
-    <div className={styles.container} >
+    <div className={styles.container}>
       <div className={styles.bannerBackground} style={{ backgroundColor: backgroundColors }}>
-        <Button textColor={backgroundColors} startDecorator={<Icon name='collections_bookmark' />}>
+        <Button textColor={backgroundColors} startDecorator={<Icon name="collections_bookmark" />}>
           Editar Coleção
         </Button>
       </div>
       <div className={styles.bannerInfo}>
         <h1>{name}</h1>
         <div className={styles.infoWrapper}>
-          <span>Criado em {createdAt}</span>
-          <span>Ultima edição em {updatedAt}</span>
+          <span>Criado em {createdAtFormatted}</span>
+          <span>Última edição em {updatedAtFormatted}</span>
         </div>
       </div>
     </div>
