2eb6cba92cff19c6750570834db57ec724c03380
douglasdans <douglas.dans@gmail.com>
2024-12-28 13:15:34 -0300

    feat(frontend | form): permite manipulação de eventos para textarea no handleChange

--- apps/frontend/src/utils/form.utils.ts
+++ apps/frontend/src/utils/form.utils.ts
@@ -1,5 +1,5 @@
 export function createHandleChange<T>(setState: (data: Partial<T>) => void) {
-  return (e: React.ChangeEvent<HTMLInputElement>) => {
+  return (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target
     setState({ [name]: value } as Partial<T>)
   }
