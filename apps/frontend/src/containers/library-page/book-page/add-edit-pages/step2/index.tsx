ed781cd3978e4c441c64e00ab0d0b8490e74a167
douglasdans <douglas.dans@gmail.com>
2024-12-29 11:32:07 -0300

    refactor(frontend | step2-book-page): altera título da seção para 'Capa e Cores do Livro'

--- apps/frontend/src/containers/library-page/book-page/add-edit-pages/step2/index.tsx
+++ apps/frontend/src/containers/library-page/book-page/add-edit-pages/step2/index.tsx
@@ -4,7 +4,7 @@ import BookCoverColorForm from './cover-color-form'
 export default function Step2BookPage() {
   return (
     <Fragment>
-      <h6>Informações do Livro</h6>
+      <h6>Capa e Cores do Livro</h6>
       <BookCoverColorForm />
     </Fragment>
   )
