4dd2300ee4c06f917c5103a29107402f3a4ad9d5
douglasdans <douglas.dans@gmail.com>
2025-01-02 12:47:17 -0300

    feat(frontend | book-banner-container): adiciona componente BookBannerContainer para exibir informações do livro

--- apps/frontend/src/app/[userId]/library/book/[bookId]/page.tsx
+++ apps/frontend/src/app/[userId]/library/book/[bookId]/page.tsx
@@ -3,6 +3,7 @@ import BookBanner from '@/components/banners/book-banner'
 import BookContentWrapper from '@/containers/library-page/book-page/main-page/book-content-container'
 import { bookApiService } from '@/api/reboo-api'
 import React, { Fragment } from 'react'
+import BookBannerContainer from '@/containers/library-page/book-page/main-page/banner-container'
 
 type Props = {
   params: {
@@ -23,7 +24,7 @@ export default async function BookPage({ params }: Props) {
 
   return (
     <Fragment>
-      <BookBanner book={book} />
+      <BookBannerContainer book={book} />
       <BookContentWrapper book={book} />
     </Fragment>
   )
